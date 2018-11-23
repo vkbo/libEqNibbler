7cfc67c2930324e6d049ea35b9e2edfc50ce8b12
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2018-11-23 23:11:34 +0100

    The example cpp file.

--- example_cpp.cpp
+++ example_cpp.cpp
@@ -4,6 +4,7 @@
  *  C++ Example Code
  */
 
+#include <time.h>
 #include <cstdlib>
 
 #include "source/libEqNibbler.hpp"
@@ -13,17 +14,24 @@ using namespace eqnibbler;
 
 int main(int argc, char const *argv[]) {
 
+    double_t theResult;
     EquationNibbler* theEQ = new EquationNibbler();
+    clock_t tStart = clock();
+    vector<string>   theVars{"a","x","y","z"};
+    // size_t idEQ = theEQ->addEquation("-(1 + (2 + x)) + (3 + (4 + y)) + (5 + (6 + (7 + z)))",theVars);
+    size_t idEQ = theEQ->addEquation("-3.2^3 + sin(pi/2) * cos(a) * exp(pi/2) - if(pi > 3, pi, 0) -(1 + (2 + x)) + (3 + (4 + y)) + (5 + (6 + (7 + z)))",theVars);
+    // size_t idEQ = theEQ->addEquation("-a + x * y / z",theVars);
+    vector<double_t> theVals{0.0,1.0,2.0,3.0};
 
-    vector<string>   theVars{"x"};
-    vector<double_t> theVals{1.0};
+    int maxItt = 20000000;
+    // int maxItt = 1;
 
-    size_t idEQ = theEQ->addEquation("-sin(x)*1e2 + (+5e+1)",theVars);
-    // size_t idEQ = theEQ->addEquation("-if(x >= 1, 1, 0)",theVars);
-
-    double_t theResult = theEQ->evalEquation(idEQ, theVals);
-
+    for(int s=0; s<maxItt; s++) {
+        theResult = theEQ->evalEquation(idEQ, theVals);
+    }
     printf("Result: %23.16e\n", theResult);
+    printf("Total time:     %.6f s\n",      (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    printf("Per operatrion: %.6f us\n", 1e6*(double)(clock() - tStart)/CLOCKS_PER_SEC/maxItt);
 
     return 0;
 }
